3edc545eeed3ab87ee9cb8c44fcd224f5d55d72c
gmlee <gmlee@ntels.com>
2022-12-21 10:23:56 +0900

    fin

--- taskmanager/lib/screens/sample_screen.dart
+++ taskmanager/lib/screens/sample_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:googleapis/calendar/v3.dart';
+import 'package:googleapis/calendar/v3.dart' as calendar;
 import 'package:taskmanager/platform/login_platform.dart';
 import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
-import 'package:googleapis_auth/googleapis_auth.dart' as auth show AuthClient;
+import "package:googleapis_auth/auth_io.dart" as auth;
+import "package:http/http.dart" as http;
 
-var googleUser = GoogleSignIn(
-    scopes: [CalendarApi.calendarScope, CalendarApi.calendarEventsScope]);
+var scopes = [
+  calendar.CalendarApi.calendarScope,
+  calendar.CalendarApi.calendarEventsScope
+];
+var googleUser = GoogleSignIn(scopes: [
+  calendar.CalendarApi.calendarScope,
+  calendar.CalendarApi.calendarEventsScope
+]);
 
 class SampleScreen extends StatefulWidget {
   const SampleScreen({Key? key}) : super(key: key);
@@ -19,7 +26,7 @@ class _SampleScreenState extends State<SampleScreen> {
   LoginPlatform _loginPlatform = LoginPlatform.none;
 
   void signInWithGoogle() async {
-    await googleUser.signIn();
+    final googleSigninAccount = await googleUser.signIn();
 
     if (googleUser != null) {
       print('$googleUser');
@@ -33,18 +40,28 @@ class _SampleScreenState extends State<SampleScreen> {
     } else {
       print("can't login");
     }
-    getCalendars();
+    getCalendars(googleSigninAccount);
   }
 
-  void getCalendars() async {
-    var url =
-        Uri.https("www.googleapis.com", "/calendar/v3/users/me/calendarList");
-    // var client = http.Client();
-    final auth.AuthClient? client = await googleUser.authenticatedClient();
-    print(await CalendarApi(client).calendarList.list(maxResults: 10));
-    //
+  void getCalendars(googleSigninAccount) async {
+    final GoogleSignInAuthentication googleAuth =
+        await googleSigninAccount.authentication;
+    auth.AccessToken token = auth.AccessToken("Bearer", googleAuth.accessToken!,
+        DateTime.now().add(const Duration(days: 1)).toUtc());
+    http.Client client = http.Client();
+
+    calendar.CalendarApi calendarApi;
+
     // var resp = await client.get(url);
     // print('body : ${resp.body}');
+    auth.AuthClient authClient = auth.authenticatedClient(
+        client, auth.AccessCredentials(token, googleSigninAccount.id, scopes));
+    calendarApi = calendar.CalendarApi(authClient);
+    try {
+      print(calendar.CalendarListEntry().id);
+    } catch (e) {
+      print(e);
+    }
   }
 
   void signOut() async {
